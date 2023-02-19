b0acd2c4abe0b6f1ff66ef2c926d0546d0088cd2
Yariv Levy <yalevy@amazon.com>
2023-02-19 20:39:50 +0200

    Initial commit

---
+++ test/ecs-blog.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsBlog from '../lib/ecs-blog-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-blog-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsBlog.EcsBlogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
